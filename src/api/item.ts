2e685589468b8ff5d184f502e26060234fcdc197
Joshua Price <joshuajangprice@gmail.com>
2024-12-20 16:00:44 -0600

    small changes

--- src/api/item.ts
+++ src/api/item.ts
@@ -1,2 +1,4 @@
-import 'GroceryItem' from '../models/GroceryItem';
-
+import { product } from '../types/db'
+import express from 'express';
+import { getProducts, createProduct, getProduct, updateProduct, deleteProduct } from '../controllers/product';
+const router = express.Router();
