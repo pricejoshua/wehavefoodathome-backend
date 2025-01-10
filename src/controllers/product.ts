055c7cf837c2ed50e26dac0b3042906c103a589b
Joshua Price <joshuajangprice@gmail.com>
2025-01-10 16:06:04 -0600

    Types and things

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -2,18 +2,17 @@ import { Request, Response } from "express";
 import supabase from "../utils/supabase";
 import { Tables } from "../types/db.types";
 
-export const getProducts = async (req: Request, res: Response): Promise<Tables<"products">> => {
+export const getProducts = async (req: Request, res: Response): Promise<Response>  => {
   const { data, error } = await supabase.from("products").select("*");
   if (error) {
     return res.status(500).json({ error: error.message });
   }
-  return res.json(data);
+  return res.status(200).json(data);
 };
 
 export const createProduct = async (req: Request, res: Response) => {
     const product = req.body as Tables<"products">;
-    const { name, price } = req.body;
-    const { data, error } = await supabase.from("products").insert({ name, price });
+    const { data, error } = await supabase.from("products").insert(product);
     if (error) {
         return res.status(500).json({ error: error.message });
     }
